38a622f64730794ea7196c505e08ffcd2f14889a
ArthurRFr <arthur_fronza2011@hotmail.com>
2024-06-08 09:30:03 -0300

    feat: add username validation on create iser

--- internal/models/user/dtos/requests.go
+++ internal/models/user/dtos/requests.go
@@ -5,5 +5,5 @@ import "github.com/google/uuid"
 type CreateUserRequestBody struct {
 	Username  string    `json:"username" gorm:"not null;unique" validate:"required"`
 	Password  string    `json:"password" gorm:"not null" validate:"required"`
-	CompanyID uuid.UUID `json:"companyID" gorm:"not null" validate:"required"`
+	CompanyID uuid.UUID `json:"companyId" gorm:"not null" validate:"required"`
 }
--- internal/models/user/handlers/user.handler.go
+++ internal/models/user/handlers/user.handler.go
@@ -31,7 +31,7 @@ func (u *userHandler) Register(w http.ResponseWriter, r *http.Request) {
 
 	err := json.NewDecoder(r.Body).Decode(&userPayload)
 	if err != nil {
-		utils.ResponseJSON(w, http.StatusBadRequest, "Invalid body")
+		utils.ResponseJSON(w, http.StatusBadRequest, err.Error())
 		return
 	}
 
@@ -39,6 +39,17 @@ func (u *userHandler) Register(w http.ResponseWriter, r *http.Request) {
 	err = validate.Struct(userPayload)
 	if err != nil {
 		utils.ResponseValidationErrors(err, w)
+		return
+	}
+
+	userAlreadyExists, usernameErr := u.userService.GetByUsername(userPayload.Username)
+	if usernameErr != nil {
+		utils.ResponseJSON(w, usernameErr.StatusCode, usernameErr.Message)
+		return
+	}
+
+	if userAlreadyExists != nil {
+		utils.ResponseJSON(w, http.StatusConflict, "User already exists")
 		return
 	}
 
