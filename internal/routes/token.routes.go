d82f4e9e2542a13ed328ff7b3d3843b677395db4
ArthurRFr <arthur_fronza2011@hotmail.com>
2024-06-15 15:23:50 -0300

    fix: remove jwt auth from token generation route

--- internal/routes/token.routes.go
+++ internal/routes/token.routes.go
@@ -4,16 +4,12 @@ import (
 	"github.com/go-chi/chi"
 
 	"na-hora/api/internal/models/token/handlers"
-	authentication "na-hora/api/internal/routes/middlewares"
 )
 
 func TokenRoutes(r chi.Router) {
 	tokenHandler := handlers.GetTokenHandler()
-	authService := authentication.NewAuthService()
 
 	r.Route("/tokens", func(r chi.Router) {
-		r.Use(authService.JwtAuthMiddleware)
-
 		r.Post("/generate", tokenHandler.GenerateRegisterLink)
 	})
 }
