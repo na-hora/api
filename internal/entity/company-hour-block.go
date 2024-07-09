861fb1ed2fbc0815d75fe8ca3b6905af299aa44f
ArthurRFr <arthur_fronza2011@hotmail.com>
2024-07-09 11:09:30 -0300

    feat: add company-hour-block table

---
+++ internal/entity/company-hour-block.go
@@ -0,0 +1,27 @@
+package entity
+
+import (
+	"time"
+
+	"github.com/google/uuid"
+	"gorm.io/gorm"
+)
+
+type CompanyHourBlock struct {
+	ID uint `json:"id" gorm:"primaryKey;autoIncrement:true"`
+
+	CompanyID uuid.UUID `json:"companyId" gorm:"not null"`
+	Day       time.Time `json:"day" gorm:"not null"`
+	StartHour float64   `json:"startHour" gorm:"not null"`
+	EndHour   float64   `json:"endHour" gorm:"not null"`
+
+	CreatedAt time.Time      `json:"createdAt" gorm:"default:CURRENT_TIMESTAMP"`
+	UpdatedAt time.Time      `json:"updatedAt" gorm:"default:CURRENT_TIMESTAMP"`
+	DeletedAt gorm.DeletedAt `json:"-" gorm:"index"`
+
+	Company Company `json:"company" gorm:"constraint:OnUpdate:CASCADE,OnDelete:SET NULL;foreignKey:CompanyID;references:ID;"`
+}
+
+func (CompanyHourBlock) TableName() string {
+	return "company_hour_block"
+}
---
+++ migrations/20240709140504_create-company-hour-block-table.sql
@@ -0,0 +1,15 @@
+-- Create "company_hour_block" table
+CREATE TABLE "public"."company_hour_block" (
+  "id" bigserial NOT NULL,
+  "company_id" uuid NOT NULL,
+  "day" timestamptz NOT NULL,
+  "start_hour" numeric NOT NULL,
+  "end_hour" numeric NOT NULL,
+  "created_at" timestamptz NULL DEFAULT CURRENT_TIMESTAMP,
+  "updated_at" timestamptz NULL DEFAULT CURRENT_TIMESTAMP,
+  "deleted_at" timestamptz NULL,
+  PRIMARY KEY ("id"),
+  CONSTRAINT "fk_company_hour_block_company" FOREIGN KEY ("company_id") REFERENCES "public"."company" ("id") ON UPDATE CASCADE ON DELETE SET NULL
+);
+-- Create index "idx_company_hour_block_deleted_at" to table: "company_hour_block"
+CREATE INDEX "idx_company_hour_block_deleted_at" ON "public"."company_hour_block" ("deleted_at");
